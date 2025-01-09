db512dcdaf48764408f483e5eae17e8ff009a370
codingkatty <m001720@permatapintar.ukm.edu.my>
2025-01-09 21:45:32 +0800

    home page catio

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# neko
+# Neko: Japanese notes because I need
+put a banner here :3
+
+So its almost Japanese exam, and I feel kinda cooked ngl. I just started learning like 10 months ago but I still can't... Japanese. Well, so I guess why not create a note website?
+
+## Contributing
+Feel free to contribute to this awesome learning page! It is very easy to do so.
+
+### What you'll need
+To contribute, you simply need to prepare a topic, a PDF is applicable and just duplicate the existing notes and start editing! The notes are pre styled, but feel free to add your own touch of flavour! For scripts, feel free to add interactive elements too, they are pretty cool.
+
+```
+notes/
+  ├── yournote.html
+pdf/
+  └── yournote.pdf
+```
+
+Then, edit the links at `line idkdothestufffirst`.
+
+## Tech Stack
---
+++ script.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (localStorage.getItem("timesPet") === null) {
+        localStorage.setItem("timesPet", 0);
+    }
+    document.getElementById("pet-mascot").innerText = localStorage.getItem("timesPet");
+
+    const mascot = document.getElementById("mascot");
+
+    mascot.addEventListener("mouseover", function() {
+        this.src = "images/neko2.png";
+    });
+
+    mascot.addEventListener("mouseout", function() {
+        this.src = "images/neko1.png";
+    });
+
+    mascot.addEventListener("click", function() {
+        const timesPet = parseInt(localStorage.getItem("timesPet")) + 1;
+        localStorage.setItem("timesPet", timesPet);
+        document.getElementById("pet-mascot").innerText = timesPet;
+    });
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 50,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ff7da0"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 10,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "bottom",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false,
+                },
+                "onclick": {
+                    "enable": false,
+                },
+                "resize": true
+            }
+        },
+        "retina_detect": true
+    });
+});

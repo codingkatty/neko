e33e1f972bbfe2497211c43df3a4210a5c86aad3
codingkatty <m001720@permatapintar.ukm.edu.my>
2025-01-13 12:47:01 +0800

    tweak links

--- notes.js
+++ notes.js
@@ -2,25 +2,25 @@ let notesList = [
     {
         title: "Countries",
         desc: "Name some countries in Japanese and create sentences.",
-        file: "notes/countries.html",
+        file: "notes/countries",
         pdf: "pdf/Countries.pdf"
     },
     {
         title: "Family",
         desc: "Learn how to say family members in Japanese.",
-        file: "notes/fam.html",
+        file: "notes/fam",
         pdf: "pdf/Family.pdf"
     },
     {
         title: "Places",
         desc: "How to say names of places in Japanese.",
-        file: "notes/places.html",
+        file: "notes/places",
         pdf: "pdf/Places.pdf"
     },
     {
         title: "Time",
         desc: "How to tell time in Japanese.",
-        file: "notes/time.html",
+        file: "notes/time",
         pdf: "pdf/Time.pdf"
     }
 ];
